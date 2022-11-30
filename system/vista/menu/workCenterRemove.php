628bb57da25e5182582c12a3416d37f6ef8184cd
Alx <segundo356alx@gmail.com>
2022-11-30 10:57:47 -0600

    Total Productivity Aplication

--- system/vista/menu/workCenterRemove.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!empty($_GET['id'])) {
-    require("php/conexion.php");
-    $id = $_GET['id'];
-    $query_delete = mysqli_query($mysqli, "DELETE FROM workcenter WHERE id_workcenter = $id");
-    mysqli_close($mysqli);
-    header("location: workCenter.php");
-}
-?>
